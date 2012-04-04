8ccd62a7671e749ea72835dece9c46ce88e71011
Ioannis Canellos <iocanel@apache.org>
2012-04-05 01:47:52 +0300

    Added completers for hardware, compute/blobstore providers.

---
+++ commands/src/main/java/org/jclouds/karaf/commands/blobstore/completer/BlobStoreProviderCompleter.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2011, the original authors
+ *
+ * ====================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ====================================================================
+ */
+
+package org.jclouds.karaf.commands.blobstore.completer;
+
+import org.apache.karaf.shell.console.Completer;
+import org.apache.karaf.shell.console.completer.StringsCompleter;
+import org.jclouds.blobstore.BlobStore;
+
+import java.util.List;
+
+public class BlobStoreProviderCompleter implements Completer {
+
+    private final StringsCompleter delegate = new StringsCompleter();
+    private List<? extends BlobStore> blobStoreServices;
+
+    @Override
+    public int complete(String buffer, int cursor, List<String> candidates) {
+        try {
+            if (blobStoreServices != null) {
+                for (BlobStore blobStore : blobStoreServices) {
+                    delegate.getStrings().add(blobStore.getContext().getProviderSpecificContext().getId());
+                }
+            }
+        } catch (Exception ex) {
+            //noop
+        }
+        return delegate.complete(buffer,cursor,candidates);
+    }
+
+    public List<? extends BlobStore> getBlobStoreServices() {
+        return blobStoreServices;
+    }
+
+    public void setBlobStoreServices(List<? extends BlobStore> blobStoreServices) {
+        this.blobStoreServices = blobStoreServices;
+    }
+}
