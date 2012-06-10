4de494565ebffbabaf5d2738cd2a730e505b32a0
Ioannis Canellos <iocanel@apache.org>
2012-06-10 17:46:27 +0300

    Added jclouds:node-info command to display more details about any given node.

---
+++ commands/src/main/java/org/jclouds/karaf/commands/compute/NodeInfoCommand.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 2011, the original authors
+ *
+ * ====================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ====================================================================
+ */
+package org.jclouds.karaf.commands.compute;
+
+import com.google.common.collect.Sets;
+import org.apache.felix.gogo.commands.Argument;
+import org.apache.felix.gogo.commands.Command;
+import org.jclouds.compute.domain.ComputeMetadata;
+import org.jclouds.compute.domain.NodeMetadata;
+import org.jclouds.compute.domain.NodeState;
+
+import java.util.List;
+
+/**
+ * @author <a href="mailto:gnodet[at]gmail.com">Guillaume Nodet (gnodet)</a>
+ */
+@Command(scope = "jclouds", name = "node-info")
+public class NodeInfoCommand extends ComputeCommandSupport {
+
+
+
+    @Argument(name = "id", description = "The id of the node.", required = true, multiValued = false)
+    private String id;
+
+    @Override
+    protected Object doExecute() throws Exception {
+        NodeMetadata node = getComputeService().getNodeMetadata(id);
+
+
+        return null;
+    }
+}
